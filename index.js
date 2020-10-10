a6e8e02df092ab79cd3b85caae844927a7cafdbf
cooljasonmelton <cooljasonmelton>
2020-10-09 21:22:59 -0500

    added counter that saves state

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // reducer is func, initialState is object
 const createStore = (reducer, initialState) => {
-    const state = initialState;
+    let state = initialState;
 
     // return current state
     const getState = () => state;
@@ -19,18 +19,74 @@ const createStore = (reducer, initialState) => {
 const reducer = (state, action) => {
     switch (action.type) {
     // saves user name
-      case 'SET_USER_NAME':
-        return {
-          ...state,
-          name: action.payload,
+        case 'SET_USER_NAME':
+            return {
+            ...state,
+            name: action.payload,
         };
     // saves light or dark
-      case 'SET_DISPLAY_MODE':
-        return {
-          ...state,
-          displayMode: action.payload,
+        case 'SET_DISPLAY_MODE':
+            return {
+            ...state,
+            displayMode: action.payload,
         };
-      default:
-        return state;
+    // saves counter number
+        case 'PLUS_ONE_COUNTER':
+            return {
+            ...state,
+            counter: action.payload,
+        };        
+        default:
+            return state;
     }
   }
+
+
+
+  // TEST RUN 
+const initialState = { 
+    name: 'Jason', 
+    displayMode: 'light', 
+    counter: 0
+};
+const store = createStore(reducer, initialState);
+
+console.log(store.getState())
+
+store.dispatch({
+    type: 'SET_USER_NAME',
+    payload: 'Dave',
+});
+
+console.log(store.getState())
+
+
+store.dispatch({
+    type: 'SET_DISPLAY_MODE',
+    payload: 'dark',
+});
+
+console.log(store.getState())
+
+
+const counterBtn = document.getElementById('counter')
+
+counterBtn.innerHTML = store.getState().counter
+
+counterBtn.addEventListener('click', () => {
+    store.dispatch({
+        type: 'PLUS_ONE_COUNTER',
+        payload: store.getState().counter + 1
+    })
+    counterBtn.innerHTML = store.getState().counter
+    console.log(store.getState())
+})
+
+
+
+
+
+
+
+
+
